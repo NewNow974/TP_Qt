eb5311b2237a609a1392310da0cd000191122a0e
NewNow974 <49619618+NewNow974@users.noreply.github.com>
2020-02-13 08:46:53 +0100

    Eloi1

---
+++ cmake-build-debug/CMakeFiles/TP_Qt.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "TP_Qt_automoc.cpp"
+  "CMakeFiles/TP_Qt.dir/main.cpp.o"
+  "CMakeFiles/TP_Qt.dir/MainWindow.cpp.o"
+  "CMakeFiles/TP_Qt.dir/TP_Qt_automoc.cpp.o"
+  "TP_Qt.pdb"
+  "TP_Qt"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/TP_Qt.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/TP_Qt_automoc.dir/cmake_clean.cmake
@@ -0,0 +1,9 @@
+file(REMOVE_RECURSE
+  "TP_Qt_automoc.cpp"
+  "CMakeFiles/TP_Qt_automoc"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/TP_Qt_automoc.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
